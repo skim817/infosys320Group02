d85f1251048c7a4053cbcd1cab1f91ce5a265fce
skim817 <skim817@aucklanduni.ac.nz>
2018-09-12 20:52:48 +1200

    a

--- ModelExplorer/Assets/Scripts/MoveObject.cs
+++ ModelExplorer/Assets/Scripts/MoveObject.cs
@@ -13,15 +13,13 @@ public class MoveObject : MonoBehaviour {
 
     void Update()
     {
-        // The step size is equal to speed times frame time.
-        float step = speed * Time.deltaTime;
-
-        // Move our position a step closer to the target.
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+     
     }
     public void Move()
     {
-        myPrefab.transform.Translate(Vector3.forward * Time.deltaTime);
+       
+            transform.Translate(Vector3.forward);
+        
     }
 
 }
